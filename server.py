2bc3ec79e02301bbfac7dea0767bc2a5f5a84d6f
briangu33 <briangu33@gmail.com>
2018-01-25 22:22:50 -0500

    gitignore pycache and check name main

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
-app.run(host='0.0.0.0')
 @app.route("/")
 def index(): 
 	return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
