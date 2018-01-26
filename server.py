cd59755da7f57136872fdfb9b59a95af650a73f8
briangu33 <briangu33@gmail.com>
2018-01-25 22:49:56 -0500

    server host thing

--- server.py
+++ server.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def index(): 
 	return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+print("running with host 0.0.0.0")
+app.run(host='0.0.0.0')
